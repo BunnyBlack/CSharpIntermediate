0fe87de99f7402629b244f62006e156936739c87
BunnyBlack <xyy6836@126.com>
2020-11-26 20:15:53 +0800

    chapter 2 update

---
+++ CSharpIntermediate/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+    public class Person
+    {
+        #region OldOne
+        // public string Name;
+        //
+        // public void Introduce(string to)
+        // {
+        //     Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        // }
+        //
+        // public static Person Parse(string str)
+        // {
+        //     var person = new Person {Name = str};
+        //     return person;
+        // }
+        //
+
+        #endregion
+
+        private DateTime _birthdate;
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            this._birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return this._birthdate;
+        }
+        
+
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpIntermediate
+﻿using System;
+
+namespace CSharpIntermediate
 {
     internal class Program
     {
@@ -6,5 +8,82 @@
         {
             
         }
+
+        public static void GetSetMethod()
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1982, 1, 1));
+            Console.WriteLine(person.GetBirthdate());
+        }
+
+        public static void ReadonlyMethod()
+        {
+            var customer = new Customer(1);
+            Console.WriteLine(customer.Orders.Count);
+            customer.Orders.Add(new Order());
+            customer.Orders.Add(new Order());
+
+            Console.WriteLine(customer.Orders.Count);
+        }
+
+        public static void TryParseMethod()
+        {
+            var number = 0;
+            var result = int.TryParse("abc", out number);
+            if (result)
+            {
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("Conversion failed!");
+            }
+        }
+
+        public static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] {1, 2, 3, 4, 5}));
+        }
+
+        public static void UsePoint()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected exception has occured!");
+            }
+        }
+
+        // public static void ObjectInitializerMethod()
+        // {
+        //     var person = new Person()
+        //     {
+        //         Name = "John"
+        //     };
+        //     person.Introduce("Mosh");
+        // }
+        //
+        
+        // public static void ClassCreateMethod()
+        // {
+        //     var person = new Person();
+        //     person.Name = "John";
+        //     person.Introduce("Mosh");
+        //
+        //     var p = Person.Parse("John");
+        //     p.Introduce("Mosh");
+        // }
     }
 }
