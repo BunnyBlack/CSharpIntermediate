892d34849f3e60088ef19b742ab5e9360e59c48a
BunnyBlack <xyy6836@126.com>
2020-11-27 15:33:10 +0800

    chapter 2 update

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -6,6 +6,11 @@ namespace CSharpIntermediate
     {
         public static void Main(string[] args)
         {
+            
+        }
+
+        public static void IndexerMethod()
+        {
             var cookie = new HttpCookie();
             cookie["name"] = "Mosh";
             Console.WriteLine(cookie["name"]);
